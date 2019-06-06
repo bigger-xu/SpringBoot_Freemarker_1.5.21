b624505790f22c6292afe4cd86aa161f1aefcd61
ZhangYongwei <562123314@qq.com>
2019-06-06 14:13:09 +0800

    页面结构

--- src/main/resources/static/js/role/index.js
+++ src/main/resources/static/js/role/index.js
@@ -1,7 +1,7 @@
 /**
  * @description
  * @author Zhang Yongwei
- * @date 2019-06-06 12:08:40
+ * @date 2019-06-06 13:44:14
  * @version 1.0
  */
 $(function () {
@@ -15,7 +15,7 @@ $("#pageCount").change(function(){
 function loadPage(page) {
     layer.load(1, {shade: [0.5, '#000']});
     $("#itemsPanel").skillAjax({
-        url: "/cms/role/page",//提交连接
+        url: "/role/page",//提交连接
         model: $("#skillModel"),
         pageModel:$("#pageModel"),
         data: {"pageCount":$("#pageCount").val()},
@@ -23,7 +23,7 @@ function loadPage(page) {
         callback: function (result) {
             layer.closeAll('loading');
             var num = result.page.startIndex;
-            if (result != null && result.page != null && result.page.rows.length > 0) {
+            if (result != null && result.page != null && result.rows.length > 0) {
                 $("#itemsPanel tr").each(function () {
                     //为序号赋值
                     $(this).find("[key=num]").append(num);
@@ -37,13 +37,13 @@ function loadPage(page) {
 }
 
 //进入修改页面
-$(document).on("click", ".update", function () {
-    var id = $(this).parent().find("#id").val();
+$(document).on("click", ".edit", function () {
+    var id = $(this).parent().find("[key=id]").val();
     window.location.href = "/role/edit?id=" + id;
 });
 //删除
 $(document).on("click", ".delete", function () {
-    var id = $(this).parent().find("#id").val();
+    var id = $(this).parent().find("[key=id]").val();
     layer.confirm('您确定要删除吗？', {
         btn: ['确定', '关闭'],//按钮
         title: false,
